98d6681db66d5e14401d07cb927b8a29e6f875f3
Jacopo Abramo <jacopo.abramo@gmail.com>
2025-01-29 13:04:13 +0100

    Use `ModelInfo` as baseclass

--- src/redsun_simulator/openwfs/_config.py
+++ src/redsun_simulator/openwfs/_config.py
@@ -1,11 +1,11 @@
-from attrs import define, field
-from sunflare.config import MotorInfo
+from attrs import define, field, setters, validators
+from sunflare.config import ModelInfo
 
 __all__ = ["OpenWFSMotorInfo"]
 
 
-@define
-class OpenWFSMotorInfo(MotorInfo):
+@define(kw_only=True)
+class OpenWFSMotorInfo(ModelInfo):
     """Configuration information for the OpenWFSMotor.
 
     See the following link_ for the base class.
@@ -14,6 +14,12 @@ class OpenWFSMotorInfo(MotorInfo):
 
     Attributes
     ----------
+    axis : ``list[str]``
+        - List of axis names.
+    step_size : ``dict[str, float]``
+        - Dictionary of axis names and their step sizes.
+    egu : ``str``
+        - Engineering units for the motor.
     setpoint_time : ``float``, optional
         - Time required to simulate the motor moving to the setpoint in seconds.
         - Default is 0.1 seconds.
@@ -23,5 +29,22 @@ class OpenWFSMotorInfo(MotorInfo):
 
     """
 
+    axis: list[str] = field(factory=list, on_setattr=setters.frozen)
+    step_size: dict[str, float] = field(factory=dict)
+    egu: str = field(validator=validators.instance_of(str), on_setattr=setters.frozen)
     setpoint_time: float = field(default=0.5)
     shutdown_time: float = field(default=0.5)
+
+    @axis.validator
+    def _validate_axis(self, _: str, value: list[str]) -> None:
+        if not all(isinstance(val, str) for val in value):
+            raise ValueError("All values in the list must be strings.")
+        if len(value) == 0:
+            raise ValueError("The list must contain at least one element.")
+
+    @step_size.validator
+    def _validate_step_size(self, _: str, value: dict[str, float]) -> None:
+        if not all(isinstance(val, float) for val in value.values()):
+            raise ValueError("All values in the dictionary must be floats.")
+        if len(value) == 0:
+            raise ValueError("The dictionary must contain at least one element.")
