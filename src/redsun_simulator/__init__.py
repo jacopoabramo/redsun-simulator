d65801b907a7a664063fe320973aa2d5f7631f54
Jacopo Abramo <jacopo.abramo@gmail.com>
2025-01-29 16:01:03 +0100

    Move imports in submodule
    - should prevent erroneous imports

--- src/redsun_simulator/__init__.py
+++ src/redsun_simulator/__init__.py
@@ -4,11 +4,3 @@ try:
     __version__ = version("redsun_simulator")
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
-
-from .openwfs import OpenWFSCameraInfo, OpenWFSMotor, OpenWFSMotorInfo
-
-__all__ = (
-    "OpenWFSMotor",
-    "OpenWFSMotorInfo",
-    "OpenWFSCameraInfo",
-)
